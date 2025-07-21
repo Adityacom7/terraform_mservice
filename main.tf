77c6af33f3be2d8d4b5bd2ab3437710ac546ec50
aditya singh <you@example.com>
2025-07-21 21:42:44 +0530

    as

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "demo-terraform-eks-state-s3-bucket"
     key            = "terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "terraform-eks-state-locks"
+    dynamodb_table = "demo-terraform-eks-lock-table"
     encrypt        = true
   }
 }
